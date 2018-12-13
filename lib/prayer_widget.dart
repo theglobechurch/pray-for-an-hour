ccfcbbd4387efdec001cc1942a0deacc328b1450
James Doc <jamesdoc@gmail.com>
2018-12-13 11:18:56 +0000

    Request the data once, not every state change

--- lib/prayer_widget.dart
+++ lib/prayer_widget.dart
@@ -13,73 +13,118 @@ class _PrayerScreenState extends State<PrayerScreen> {
   bool _moveOn = false;
   int maxValue = 11;
   var t;
+  var _prayerData;
 
   @override
   void initState() {
+    super.initState();
+    _prayerData = loadPrayer();
     _countdown();
   }
 
   // Go to the next screen
   void _nextScreen() {
     if (_i < maxValue) {
-
       setState(() {
         _i++;
         _moveOn = false;
       });
       
+      // Restart the timer
       _countdown();
     }
   }
 
   _countdown() {
-    t = new Timer(Duration(seconds: 10), (){
+    t = new Timer(Duration(seconds: 2), (){
       _timerComplete();
     });
   }
 
   _timerComplete() {
     debugPrint('⏰ Timer Complete');
-
-    setState(() {
-      _moveOn = true;
-    });
+    setState((){ _moveOn = true; });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Prayer…"),
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: FutureBuilder<dynamic>(
-          future: loadPrayer(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              maxValue = snapshot.data.prayers.length - 1;
-              return Text(
-                  '${snapshot.data.prayers[_i].name}: ${snapshot.data.prayers[_i].body}');
-            } else {
-              return CircularProgressIndicator();
-            }
-          }
-        ),
-      ),
-      floatingActionButton: _moveOn ? FloatingActionButton(
-        onPressed: !_moveOn ? null : () {
-          if (_i >= maxValue) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => EndCardScreen()),
-            );
-          } else {
-            _nextScreen();
-          }
-        },
-        child: new Icon(Icons.navigate_next)
-      ) : null
+    return FutureBuilder<dynamic>(
+      future: _prayerData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+
+          // This should not be here…
+          // and where this goes the first call to _countdown() belongs
+          maxValue = snapshot.data.prayers.length - 1;
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(snapshot.data.prayers[_i].name),
+              automaticallyImplyLeading: false,
+            ),
+            body: Center(
+              child: Text(snapshot.data.prayers[_i].body),
+            ),
+            floatingActionButton: _moveOn ? FloatingActionButton(
+              onPressed: !_moveOn ? null : () {
+                if (_i >= maxValue) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => EndCardScreen()),
+                  );
+                } else {
+                  _nextScreen();
+                }
+              },
+              child: new Icon(Icons.navigate_next)
+            ) : null
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Loading…"),
+              automaticallyImplyLeading: false,
+            ),
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      }
     );
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text("Prayer…"),
+    //     automaticallyImplyLeading: false,
+    //   ),
+    //   body: Center(
+    //     child: FutureBuilder<dynamic>(
+    //       future: _prayerData,
+    //       builder: (context, snapshot) {
+    //         if (snapshot.hasData) {
+    //           maxValue = snapshot.data.prayers.length - 1;
+    //           return Text(
+    //               '${snapshot.data.prayers[_i].name}: ${snapshot.data.prayers[_i].body}');
+    //         } else {
+    //           return CircularProgressIndicator();
+    //         }
+    //       }
+    //     ),
+    //   ),
+    //   floatingActionButton: _moveOn ? FloatingActionButton(
+    //     onPressed: !_moveOn ? null : () {
+    //       if (_i >= maxValue) {
+    //         Navigator.push(
+    //           context,
+    //           MaterialPageRoute(builder: (context) => EndCardScreen()),
+    //         );
+    //       } else {
+    //         _nextScreen();
+    //       }
+    //     },
+    //     child: new Icon(Icons.navigate_next)
+    //   ) : null
+    // );
   }
 }
--- lib/services/prayer_services.dart
+++ lib/services/prayer_services.dart
@@ -4,10 +4,12 @@ import 'dart:convert';
 import 'package:pray_for_an_hour/models/prayer_model.dart';
 
 Future<String> _loadPrayerAsset() async {
-  return await rootBundle.loadString('assets/data/default.json');
+  return await rootBundle.loadString('assets/data/dev.json');
+  // return await rootBundle.loadString('assets/data/default.json');
 }
 
 Future loadPrayer() async {
+  print('👋 Requested data');
   String jsonPrayer = await _loadPrayerAsset();
   final jsonResponse = json.decode(jsonPrayer);
   PrayersList prayersList = PrayersList.fromJson(jsonResponse);
